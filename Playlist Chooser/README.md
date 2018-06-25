ed8d67603f46775f2c5c43f7df7de41f8d74468e
Arjun Chattoraj <arjunchattoraj10@gmail.com>
2018-06-25 11:30:05 -0400

    playlist folder added

---
+++ Playlist Chooser/README.md
@@ -0,0 +1,9 @@
+# README
+
+This is a playlist chooser.
+
+Choices are tough, so I made a program that contains the names of all my playlists, so that the computer can make the choice for me.
+
+There is also an option called *other*. If the computer randomly chooses other, it then chooses among those.
+*Other* choices include either a specific artist, or a playlist of a bunch of artists. 
+However, these playlists are randomly put together and are not meant to be played in order, but rather shuffled.
---
+++ Playlist Chooser/playlist.py
@@ -0,0 +1,38 @@
+import random
+
+# a list of playlists I have on Amazon Music. This list will be updated as more playlists are added.
+playlists = [
+			"Kiera's Grandmaster Billy Joel Playlist for Instant Life Improvement",
+			"The Definite Essential Mainstream Music of Billy Joel",
+			"The Essential and Most Genius David Bowie, Culled by Me, Kiera Kiera Allen",
+			"Tormey Tunes",
+			"Love Beatles",
+			"Oakland Coffee",
+			"Green Day and more",
+			"Other"
+			]
+
+# a list of "other" music.
+other = [
+		# amalgamation playlists
+		"Artistic Amalgamation",
+		"Davie Mercury",
+		# specific artists
+		"Green Day",
+		"The Beatles",
+		"Imagine Dragons",
+		"Billy Joel",
+		# all songs
+		"Arjun Plays"
+		]
+
+playlist_choice = random.choice(playlists)
+
+
+print("\nToday you will be listening to: " + playlist_choice)
+
+if playlist_choice == "Other":
+	other_choice = random.choice(other)
+	print("Among the other choices, you will be shuffling through: " + other_choice)
+
+print("Enjoy your listening!")
